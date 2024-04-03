56ff61bb6c5554064d88e1f6e6018b224cbe387f
hyunwoo89 <144410225+hyunwoo89@users.noreply.github.com>
2024-04-03 12:07:41 +0700

    Start

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+
+    public class Bill
+    {
+        public List<Product> Products = new List<Product>();
+        
+        public void AddProductList(Product _pd)
+        {
+            Products.Add(_pd);
+        }
+        public void PrintProductList()
+        {
+            foreach (Product _pd in Products)
+            {
+                Console.Write("Name :");
+                Console.WriteLine(_pd.Name);
+                Console.Write("Price :");
+                Console.WriteLine(_pd.Price);
+            }
+        }
+    }
+
+
+    private static void Main(string[] args)
+    {
+        Bill bill = new Bill();
+        bill.AddProductList(new Product("1",1));
+        bill.AddProductList(new Product("2", 3));
+        bill.PrintProductList();
+
+        //Console.WriteLine("Hello, World!");
+    }
+}
